4626413e2accb3f838be46eba37a93af1a591c67
WesleyLaura <wrlaurac@unjbg.edu.pe>
2025-09-16 23:40:03 -0500

    corrigiendo el ejercicio 7 , modificando lo que estaba dentro del sistema  lambda

--- Sistema de ecuaciones  No lienales/No linela de 3 variables/Ejercicio7.py
+++ Sistema de ecuaciones  No lienales/No linela de 3 variables/Ejercicio7.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 
 sistema = lambda v :[
-    mp.exp(v[0]) + v[1] + v[2] - 4,
-    v[0] + mp.exp(v[1]) + v[2] - 5,
-    v[0] + v[1] + mp.exp(v[2]) - 6
+    mp.exp(v[0]) + v[1]**2 + v[2] - 7,
+    v[0] + mp.exp(v[1]) + v[2]**2 - 8,
+    v[0]**2 + v[1] + mp.exp(v[2]) - 9
     
 ]
 
